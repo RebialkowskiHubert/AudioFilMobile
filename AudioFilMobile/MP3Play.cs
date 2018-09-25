60b7f48ada0a43f382ee835776137146e23a3285
Hubert Rebialkowski <hubertrebialkowski@gmail.com>
2018-09-25 18:23:00 +0200

    Implementation play and stop function

---
+++ AudioFilMobile/MP3Play.cs
@@ -0,0 +1,46 @@
+﻿using Android.OS;
+using Android.Views;
+using System;
+
+namespace AudioFilMobile
+{
+    public class MP3Play : AsyncTask<string, string, string>
+    {
+        private MainActivity mainActivity;
+
+        public MP3Play(MainActivity mainActivity)
+        {
+            this.mainActivity = mainActivity;
+        }
+
+        protected override void OnPreExecute()
+        {
+            mainActivity.progressBar.Visibility = ViewStates.Visible;
+            base.OnPreExecute();
+        }
+
+        protected override string RunInBackground(params string[] @params)
+        {
+            try
+            {
+                mainActivity.mediaPlayer.SetDataSource(@params[0]);
+                mainActivity.mediaPlayer.Prepare();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return null;
+        }
+
+        protected override void OnPostExecute(string result)
+        {
+            if (!mainActivity.mediaPlayer.IsPlaying)
+            {
+                mainActivity.mediaPlayer.Start();
+            }
+            mainActivity.progressBar.Visibility = ViewStates.Invisible;
+        }
+    }
+}
--- AudioFilMobile/MainActivity.cs
+++ AudioFilMobile/MainActivity.cs
@@ -1,19 +1,41 @@
 ﻿using Android.App;
+using Android.Media;
 using Android.OS;
 using Android.Support.V7.App;
-using Android.Runtime;
+using Android.Views;
 using Android.Widget;
+using static Android.Media.MediaPlayer;
+using static Android.Views.View;
 
 namespace AudioFilMobile
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/Theme.AppCompat.Light.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
-    {
+    { 
+        public ImageButton btn_play, btn_stop;
+        public ProgressBar progressBar;
+        public MediaPlayer mediaPlayer;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            progressBar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
+
+            btn_play = FindViewById<ImageButton>(Resource.Id.btn_play);
+            btn_play.Click += (s, e) =>
+            {
+                mediaPlayer = new MediaPlayer();
+                new MP3Play(this).Execute("http://poznan7.radio.pionier.net.pl:8000/tuba9-1.mp3");
+            };
+
+            btn_stop = FindViewById<ImageButton>(Resource.Id.btn_stop);
+            btn_stop.Click += (s, e) =>
+            {
+                mediaPlayer.Stop();
+                mediaPlayer = null;
+            };
         }
     }
 }
